dbc6c57f87ebe8d9e76349d8a912353de3e6f1f3
Tommy Leung <me@supertommy.com>
2020-01-18 10:13:21 -0700

    update for phaser 3.22

---
+++ @types/phaser/index.d.ts
@@ -0,0 +1,3 @@
+//// <reference path="../../node_modules/phaser/types/SpineFile.d.ts" />
+/// <reference path="../../node_modules/phaser/types/SpineGameObject.d.ts" />
+/// <reference path="../../node_modules/phaser/types/SpinePlugin.d.ts" />
